9eae75f074d87cedde6dca9204553e0d5b24b9d3
BarathWorks <barath.sparkit@gmail.com>
2026-01-06 18:33:50 +0530

    optimize the blog page to prevent crash

--- apps/vyan-client/src/app/(blogs)/blogs.tsx
+++ apps/vyan-client/src/app/(blogs)/blogs.tsx
@@ -1,74 +1,8 @@
-
 import SectionTitle from "~/components/shared/section-title";
-import BlogSlider from "./blogs-slider";
-import { db } from "~/server/db";
 import Slider from "./blogs-slider";
-export const dynamic = 'force-dynamic';
-// const blogCredentials = [
-//   {
-//     src: "/images/blogs/blog1.png",
-//     date: "Posted on : 14 Jan 2024",
-//     heading: "Know your carbs and diet balance in easy way",
-//     description:
-//       "Recognizing organizations as collections of human beings who are motivated by varying perspectives and emotions",
-//   },
-//   {
-//     src: "/images/blogs/blog2.png",
-//     date: "Posted on : 14 Jan 2024",
-//     heading: "Know your carbs and diet balance in easy way",
-//     description:
-//       "Recognizing organizations as collections of human beings who are motivated by varying perspectives and emotions",
-//   },
-//   {
-//     src: "/images/blogs/blog3.png",
-//     date: "Posted on : 14 Jan 2024",
-//     heading: "Know your carbs and diet balance in easy way",
-//     description:
-//       "Recognizing organizations as collections of human beings who are motivated by varying perspectives and emotions",
-//   },
-//   {
-//     src: "/images/blogs/blog1.png",
-//     date: "Posted on : 14 Jan 2024",
-//     heading: "Know your carbs and diet balance in easy way",
-//     description:
-//       "Recognizing organizations as collections of human beings who are motivated by varying perspectives and emotions",
-//   },
-//   {
-//     src: "/images/blogs/blog2.png",
-//     date: "Posted on : 14 Jan 2024",
-//     heading: "Know your carbs and diet balance in easy way",
-//     description:
-//       "Recognizing organizations as collections of human beings who are motivated by varying perspectives and emotions",
-//   },
-//   {
-//     src: "/images/blogs/blog3.png",
-//     date: "Posted on : 14 Jan 2024",
-//     heading: "Know your carbs and diet balance in easy way",
-//     description:
-//       "Recognizing organizations as collections of human beings who are motivated by varying perspectives and emotions",
-//   },
-//   {
-//     src: "/images/blogs/blog1.png",
-//     date: "Posted on : 14 Jan 2024",
-//     heading: "Know your carbs and diet balance in easy way",
-//     description:
-//       "Recognizing organizations as collections of human beings who are motivated by varying perspectives and emotions",
-//   },
-//   {
-//     src: "/images/blogs/blog2.png",
-//     date: "Posted on : 14 Jan 2024",
-//     heading: "Know your carbs and diet balance in easy way",
-//     description:
-//       "Recognizing organizations as collections of human beings who are motivated by varying perspectives and emotions",
-//   },
-//   {
-//     src: "/images/blogs/blog3.png",
-//     date: "Posted on : 14 Jan 2024",
-//     heading: "Know your carbs and diet balance in easy way",
-//     description:
-//       "Recognizing organizations as collections of human beings who are motivated by varying perspectives and emotions",
-//   },
-// ];
+import { db } from "~/server/db";
+
+export const dynamic = "force-dynamic";
 
 const Blogs = async () => {
   const blogCredentials = await db.blog.findMany({
@@ -84,27 +18,33 @@ const Blogs = async () => {
       body: true,
       createdAt: true,
       slug: true,
-      shortDescription:true,
+      shortDescription: true,
     },
     where: {
       active: true,
     },
   });
-  return (
-    <>
-      <div className="w-full py-8 md:py-[55px] xl:py-[65px] ">
-        <div className="container mx-auto max-w-full">
-          <SectionTitle text="Blogs" />
-          <div className="mb-8 text-center font-inter text-base font-normal md:mb-[30px] lg:mb-[38px] xl:mb-[44px] 2xl:mb-[48px] 2xl:text-lg 2xl:font-medium">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
-          </div>
 
-          {/* <BlogSlider blogCredentials={blogCredentials} /> */}
-          <Slider blogCredentials={blogCredentials}/>
-        </div>
+  if (blogCredentials.length === 0) {
+    return (
+      <div className="w-full py-8 text-center">
+        <SectionTitle text="Blogs" />
+        <p>No blogs available right now.</p>
       </div>
-    </>
+    );
+  }
+
+  return (
+    <div className="w-full py-8 md:py-[55px] xl:py-[65px]">
+      <div className="container mx-auto max-w-full">
+        <SectionTitle text="Blogs" />
+        <div className="mb-8 text-center">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </div>
+        <Slider blogCredentials={blogCredentials} />
+      </div>
+    </div>
   );
 };
+
 export default Blogs;
