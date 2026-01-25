0b4895e7671b31239db0fc9a7132aafe455a6b0d
BarathWorks <chidambaramb2@gmail.com>
2026-01-25 13:18:55 +0530

    ui improvement

--- apps/vyan-client/src/components/hero.tsx
+++ apps/vyan-client/src/components/hero.tsx
@@ -4,25 +4,17 @@ import { InteractiveButton } from "./ui/interactive-button";
 
 export default function Hero(): JSX.Element {
   return (
-    <section className="relative  h-[500px] w-full overflow-hidden bg-white sm:h-[600px] md:h-[700px] lg:h-[800px] xl:h-[880px]">
-      {/* Background Circle - Always 40vw responsive */}
+    <section className="relative  h-[500px] w-full overflow-hidden bg-white sm:h-[600px] md:h-[700px] lg:h-[800px] xl:h-[880px] pl-2 md:pl-12 pt-12 pb-8 lg:pl-12 lg:pt-12 lg:pb-12">
       <div
-        className="absolute right-[-35vw] top-[50%] z-20 hidden translate-y-[-50%] rounded-full bg-[#9D9D8D] md:block"
-        style={{ width: "80vw", height: "80vw" }}
-      />
-
-      <div
-        className="absolute left-0 top-[25%] z-30
-        flex w-full -translate-y-1/2 flex-col
-        gap-4 sm:left-0 sm:w-full
-        md:left-0 md:max-w-[55%] lg:left-0 lg:max-w-[50%] xl:left-0 xl:max-w-[55%] 2xl:max-w-[1000px]"
+        className=""
       >
         <h1
           className="font-poppins text-[32px] font-bold
           leading-tight text-[#114668] sm:text-[44px] md:text-[48px]
           lg:text-[56px] xl:text-[72px] 2xl:text-[88px]"
         >
-          Empowering <span className="text-[#51AF5A] font-epicgant">Women</span>, <br />
+          Empowering <span className="font-epicgant text-[#51AF5A]">Women</span>
+          , <br />
           Nurturing Families
         </h1>
 
@@ -36,32 +28,18 @@ export default function Hero(): JSX.Element {
           every stage of womanhood.
         </p>
       </div>
-
-      {/* Hero Image - Always 40% of screen width */}
       <div
-        className="absolute right-0 top-0 z-30 hidden h-full md:block"
-        style={{ width: "40%" }}
-      >
-        <img
-          src="/home/hero.png"
-          alt="SheFit Hero"
-          className="h-full w-full object-contain object-right-top transition-transform duration-500 hover:scale-[1.02]"
-        />
-      </div>
-
-      <div
-        className="pointer-events-none absolute bottom-[18%] left-0 select-none
+        className="pointer-events-none   
         bg-[linear-gradient(180deg,#114668_1%,#FFFFFF_85%)] bg-clip-text
-        font-poppins text-[80px]
+        font-poppins line-height-[0.5] text-[100px]
         font-semibold text-transparent
-        opacity-20 sm:left-0 sm:text-[100px] sm:bottom-[18%] md:text-[120px] md:bottom-[20%]
-        lg:left-0 lg:text-[180px] lg:bottom-[22%] xl:left-0 xl:text-[210px] xl:bottom-[24%] 2xl:text-[240px]"
+        opacity-20  lg:left-0 lg:text-[180px] xl:left-0 xl:text-[210px] 2xl:text-[220px]"
       >
         #shewell
       </div>
 
       {/* CTA Buttons - Responsive positioning */}
-      <div className="absolute bottom-12 left-0 z-40 flex flex-wrap items-center gap-4 sm:bottom-14 sm:left-0 md:bottom-16 md:left-0 lg:bottom-20 xl:bottom-24 xl:left-0">
+      <div className=" bottom-12 left-0 z-40 flex flex-wrap items-center gap-4 sm:bottom-14 sm:left-0 md:bottom-16 md:left-0 lg:bottom-20 xl:bottom-24 xl:left-0">
         <Link href="/counselling">
           <div className="order-0 group flex h-[80px] w-auto flex-none flex-grow flex-row items-center justify-between gap-2.5 rounded-2xl bg-[#F2F2F2] px-6 py-4 transition-all duration-300 ease-in-out hover:bg-[#e5e5e5]">
             <span className="text-lg font-medium text-[#00000066] sm:text-xl">
@@ -78,6 +56,22 @@ export default function Hero(): JSX.Element {
             <InteractiveButton />
           </div>
         </Link>
+      </div>
+      {/* Background Circle - Always 40vw responsive */}
+      <div
+        className="absolute right-[-35vw] top-[50%] z-20 hidden translate-y-[-50%] rounded-full bg-[#9D9D8D] md:block"
+        style={{ width: "80vw", height: "80vw" }}
+      />
+      {/* Hero Image - Always 40% of screen width */}
+      <div
+        className="absolute right-0 top-0 z-30 hidden h-full md:block"
+        style={{ width: "40%" }}
+      >
+        <img
+          src="/home/hero.png"
+          alt="SheFit Hero"
+          className="h-full w-full object-contain object-right-top transition-transform duration-500 hover:scale-[1.02]"
+        />
       </div>
     </section>
   );
